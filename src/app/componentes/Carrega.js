93492e474519623295859ff70a1a19512eddf65f
MaryaLauraRS <117856098+MaryaLauraRS@users.noreply.github.com>
2024-07-24 22:20:31 -0400

    Create Carrega.js
    criando o spinner

---
+++ src/app/componentes/Carrega.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Carrega(){
+    return (
+
+        <Image src="/3-dots-scale-middle.svg" alt='Imagem carregando' width={100} height={100}/>
+    );
+}
