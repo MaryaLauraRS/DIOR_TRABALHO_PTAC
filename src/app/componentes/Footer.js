2cbb8cfa9f1aad225f5380f6807d288a8f204c34
MaryaLauraRS <117856098+MaryaLauraRS@users.noreply.github.com>
2024-07-24 22:14:18 -0400

    Create Footer.js
    Criação Footer.js: Rodapé da página, que exibe o logo "DIOR", a estilização é feita pelo CSS do Footer.js

---
+++ src/app/componentes/Footer.js
@@ -0,0 +1,14 @@
+import Styles from "./main.module.css";
+
+export default async function Footer() {
+    return (
+      <footer>
+        <div className={Styles.footer}>
+          <br/>
+            <img  src="https://lojadior.vtexassets.com/assets/vtex/assets-builder/lojadior.store-theme/1.0.70/svg/logo-mobile___5ad97bb9b479734514bf4909a98581de.svg"  width="120" height="41"/>
+            <br/>
+        </div>
+      </footer>
+    );
+  }
+  
